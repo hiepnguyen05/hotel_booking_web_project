8a4beafd28979322edc8a5e8132beb1483e7b466
hiepnguyen05 <hiep20122005@gmail.com>
2025-10-21 19:33:47 +0700

    Update environment variables for production deployment

--- back_end/config/server.config.js
+++ back_end/config/server.config.js
@@ -2,6 +2,8 @@ require("dotenv").config();
 
 // Log NGROK_URL for debugging
 console.log('[SERVER CONFIG] NGROK_URL from env:', process.env.NGROK_URL);
+// Log MONGO_URI for debugging (but don't log the full URI for security)
+console.log('[SERVER CONFIG] MONGO_URI from env:', process.env.MONGO_URI ? 'MONGO_URI is set' : 'MONGO_URI is not set');
 
 module.exports = {
     PORT: process.env.PORT || 5000,
